61a45cbb813c2e7c0e4de7ea008805d660ddd514
Konstantin Bondar <bondar.k.dev@gmail.com>
2024-01-18 19:22:06 +0100

    Initial Commit

---
+++ BirthdayTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayTestApp
+//
+//  Created by Konstantin Bondar on 18.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
